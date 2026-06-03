cc9c341610f5e70b314d0934048b314c27caa391
MinJantek <antek.sowy@gmail.com>
2026-06-03 21:45:10 +0200

    Bóg z przyszłości (testy)

--- GitHub_tests.py
+++ GitHub_tests.py
@@ -1,2 +1,4 @@
-#lubię placki
-#i siebie
+rok = int(input("Podaj swój rok urodzenia: "))
+LATA_PO_URODZENIU_CHRYSTUSA = 2026
+if rok > LATA_PO_URODZENIU_CHRYSTUSA:print("Jesteś bogiem z przyszłości!!!!")
+else:print(f"Masz {LATA_PO_URODZENIU_CHRYSTUSA - rok} lat.");
