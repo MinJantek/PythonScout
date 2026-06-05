788670416df203c49ee63eb17335d6e7adcc30df
MinJantek <antek.sowy@gmail.com>
2026-06-06 00:24:01 +0200

    java jdk

---
+++ Java_folder/cos.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class cos {
+    public static void main(String[] args) {   
+        Scanner scann = new Scanner(System.in);
+        System.out.print("Podaj rok urodzenia: ");
+        int rok = scann.nextInt();
+        int lata_po_urodzeniu_chrystusa = 2026;
+        if (rok > lata_po_urodzeniu_chrystusa) {
+            System.out.println("Jesteś bogiem z przyszłości!!!!");
+        }
+        else {
+
+            int wiek = lata_po_urodzeniu_chrystusa - rok;
+
+            System.out.println("Masz " + wiek + " lat.");
+        }
+        scann.close();
+
+
+    }
+}
--- Java_folder/sciaga/funkcje.java
+++ Java_folder/sciaga/funkcje.java
@@ -1,54 +1,5 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        // printy
-        System.out.println("Hello world");
-        System.out.print("Hello world");
-        System.out.printf("%-10s", "Hello world");
-        // System.out.printf(typ (%s)+ile(-10 = 10 cyfr od lewj), tekst tego typu ("Hello world"));
-        // zmienne
-        int a = 5; 
-        double b = 3.14;
-        String c = "Hello world";
-        char d = 'A';
-        boolean e = true;
-        // operatory   
-        int f = a + 10; // dodawanie
-        int g = a - 2; // odejmowanie  
-        int h = a * 3; // mnożenie
-        double i = b / 2; // dzielenie
-        int j = a % 2; // reszta z dzielenia
-        boolean k = a > 3; // porównanie
-        boolean l = a == 5; // porównanie równości
-        boolean m = a != 5; // porównanie nierówności
-        // instrukcje warunkowe
-        if (a > 3) {
-            pass; // kod do wykonania, jeśli warunek jest prawdziwy
-        } else if (a == 3) {
-            pass; // kod do wykonania, jeśli warunek jest prawdziwy
-        } else {
-            pass; // kod do wykonania, jeśli żaden z powyższych warunków nie jest prawdziwy
-        }
-        // pętle
-        for (int i = 0; i < 10; i++) {// od ilu, do ilu, co ile; i++ = i = i + 1 = i += 1
-            pass; // kod do wykonania w każdej iteracji
-        }
-        while (a < 10) {
-            // kod do wykonania, dopóki warunek jest prawdziwy
-            a++; // zwiększanie a o 1, aby uniknąć nieskończonej pętli
-        }
-
-        do {//odwrócona pętla while, najpierw wykonuje kod, potem sprawdza warunek
-            // kod do wykonania, dopóki warunek jest prawdziwy
-            System.out.println(a);
-            a++; // zwiększanie a o 1, aby uniknąć nieskończonej pętli
-        } while (a < 10);
-
-
-
-
-
-
-
+public class funkcje {
+    public static int dodaj(int x, int y) {
+            return x + y; // zwraca sumę x i y
     }
 }
