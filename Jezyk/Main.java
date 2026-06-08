b858bfacfb288eaee24e8ee5b935f05938b6e467
MinJantek <antek.sowy@gmail.com>
2026-06-08 15:07:17 +0200

    a

--- Jezyk/Main.java
+++ Jezyk/Main.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scann = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Podaj rok urodzenia: ");
-        int rok = scann.nextInt();
+        int rok = scanner.nextInt();
         int lata_po_urodzeniu_chrystusa = 2026;
         if (rok > lata_po_urodzeniu_chrystusa) {
-            System.out.println("Jesteś bogiem z przyszłości!!!!");
+            System.out.print("Jesteś bogiem z przyszłości!!!!");
         }
         else {
-
             int wiek = lata_po_urodzeniu_chrystusa - rok;
-
-            System.out.println("Masz " + wiek + " lat.");
-        scann.close();
+            System.out.print("Masz " + wiek + " lat.");
+        scanner.close();
         }
 
     }
 }
+ 
